a7667deb834071dc824bf0f9f54074bf3f0b7769
Rohit Salitra <rohit.salitra07@gmail.com>
2024-02-06 12:27:42 +0530

    uncomment js

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -1,16 +1,16 @@
-// App.chat = App.cable.subscriptions.create("ChatChannel", {
-//   received: function(data) {
-//     $('#messages').append("<p>" + data.message + "</p>");
-//   },
+App.chat = App.cable.subscriptions.create("ChatChannel", {
+  received: function(data) {
+    $('#messages').append("<p>" + data.message + "</p>");
+  },
   
-//   send_message: function(message) {
-//     this.perform('receive', { message: message });
-//   }
-// });
+  send_message: function(message) {
+    this.perform('receive', { message: message });
+  }
+});
 
-// $(document).on('submit', '#new_message', function(e) {
-//   e.preventDefault();
-//   var message = $('#message_content').val();
-//   $('#message_content').val('');
-//   App.chat.send_message(message);
-// });
+$(document).on('submit', '#new_message', function(e) {
+  e.preventDefault();
+  var message = $('#message_content').val();
+  $('#message_content').val('');
+  App.chat.send_message(message);
+});
